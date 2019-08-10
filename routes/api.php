8806f7c0d04281f95fe0f8b8c6bd9f6a0503bdaf
farsheel <farsheelpkl@gmail.com>
2019-08-10 22:31:00 +0530

    auto deploy

--- routes/api.php
+++ routes/api.php
@@ -18,6 +18,8 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
 
+Route::post('deploy', 'Api\DeployController@deploy');
+
 
 Route::post('login', 'Api\ApiAuthController@login');
 Route::post('signup', 'Api\ApiAuthController@signup');
